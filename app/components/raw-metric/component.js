c672bfe04cefdebed54126a251bfb14860a592ae
edborden <borden.edward@gmail.com>
2016-03-17 14:08:27 -0400

    colors tweaks

--- app/components/raw-metric/component.js
+++ app/components/raw-metric/component.js
@@ -12,6 +12,7 @@ export default Component.extend({
     Keen.ready(() => {
       let chart = new Keen.Dataviz();
       chart.el(this.get('element'));
+      chart.colors(['#852667']);
       chart.prepare();
       chart.data({ result: this.get('metric') });
       chart.render();
